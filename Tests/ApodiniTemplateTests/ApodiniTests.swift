f13fd771ac0bdbb44861c2ecfe328a4b3469ea57
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2021-11-02 17:59:36 +0100

    Update tests

--- Tests/ApodiniTemplateTests/ApodiniTests.swift
+++ Tests/ApodiniTemplateTests/ApodiniTests.swift
@@ -11,13 +11,23 @@ import XCTest
 
 
 final class ApodiniTemplateTests: XCTestCase {
-    func testExample() async throws {
+    // Unfortunately, Swift on Linux does not support async tests at the moment. Therefore we use the
+    // workaround creating a Task and an expectation to wait for the completion of the async functions:
+    func testExample() throws {
         let template = ApodiniTemplate()
         
-        let firstGreeting = try await template.greet()
-        XCTAssertEqual(firstGreeting, "Hello, Apodini Template!")
-        
-        let secondGreeting = try await template.greet("Paul")
-        XCTAssertEqual(secondGreeting, "Hello, Paul!")
+        let expectation = XCTestExpectation(description: "Async Task completion")
+
+        Task {
+            defer { expectation.fulfill() }
+
+            let firstGreeting = try await template.greet()
+            XCTAssertEqual(firstGreeting, "Hello, Apodini Template!")
+            
+            let secondGreeting = try await template.greet("Paul")
+            XCTAssertEqual(secondGreeting, "Hello, Paul!")
+        }
+
+        wait(for: [expectation], timeout: 1.1)
     }
 }
